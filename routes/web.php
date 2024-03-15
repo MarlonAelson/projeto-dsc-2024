ecaf32b142f18c86921994266e18d4e573ac8b58
marlon ifrn <marlon.aelson.gomes@gmail.com>
2024-03-15 20:31:59 -0300

    criacao de controller e migration

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+/*
 Route::get('/novo-arquivo', function () {
     return view('teste');
-});
+});*/
+
+Route::get('/novo-arquivo', [AlunoController::class, 'index']);
